4c0fee5d51d5c8b2a5bd46d89376d95340ea40da
SomeOne1305 <axmadxolmuminov2007@gmail.com>
2022-09-09 22:55:32 +0500

    almost finished

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 document.querySelector('.result').addEventListener('click', ()=>{
     document.querySelector('.languagesList').classList.toggle('visible')
 })
+document.querySelector('#search').addEventListener('click', ()=>{
+    document.querySelector('.SearchResults').classList.toggle('visible')
+})
